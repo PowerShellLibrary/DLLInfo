f26eb9c3e895b10134b962504fc6e01f39f225c5
Alan Płócieniak <alan-null@outlook.com>
2019-03-14 01:02:24 +0100

    Added Get-TargetCPU cmdlet Removed elements permitted by the restricted language Removed formatting files

--- DLLInfo/DLLInfo.psd1
+++ DLLInfo/DLLInfo.psd1
@@ -3,12 +3,11 @@
     RootModule        = 'DLLInfo.psm1'
     ModuleVersion     = '1.0'
     GUID              = 'cf990a96-889f-4c16-9681-d92c592be539'
-    Author            = 'Alan Płócieniak'
-    CompanyName       = 'Alan Płócieniak'
-    Copyright         = '(c) 2019 Alan Płócieniak. All rights reserved.'
+    Author            = 'Alan Plocieniak'
+    CompanyName       = 'Alan Plocieniak'
+    Copyright         = '(c) 2019 Alan Plocieniak. All rights reserved.'
     Description       = 'Read information about DLL file'
     PowerShellVersion = '3.0'
-    FormatsToProcess  = 'DLLInfo.Format.ps1xml'
     FunctionsToExport = '*'
     PrivateData       = @{
         PSData = @{
--- Tests/DLLInfo.Tests.ps1
+++ Tests/DLLInfo.Tests.ps1
@@ -66,3 +66,34 @@ Describe 'Test-JitOptimized' {
         }
     }
 }
+
+Describe 'Get-TargetCPU' {
+    Context "Check test DLLs - x64" {
+        It "Given valid -Assembly '<Assembly>', it returns <Expected>" -TestCases @(
+            @{ Assembly = "$PSScriptRoot\bin\Debug\AssemblyInfoTest.dll"; Expected = 'AnyCPU' }
+            @{ Assembly = "$PSScriptRoot\bin\Release\AssemblyInfoTest.dll"; Expected = 'AnyCPU' }
+            @{ Assembly = "$PSScriptRoot\bin\x64\Debug\AssemblyInfoTest.dll"; Expected = 'AMD64' }
+            @{ Assembly = "$PSScriptRoot\bin\x64\Release\AssemblyInfoTest.dll"; Expected = 'AMD64' }
+        ) {
+            param ($Assembly, $Expected)
+            Get-TargetCPU $Assembly | Should -Be $Expected
+        }
+    }
+
+    Context "Check test DLLs - x86" {
+        It "Given valid -Assembly '<Assembly>', it returns <Expected>" -TestCases @(
+            @{ Assembly = "$PSScriptRoot\bin\x86\Debug\AssemblyInfoTest.dll"; Expected = 'Intel32' }
+            @{ Assembly = "$PSScriptRoot\bin\x86\Release\AssemblyInfoTest.dll"; Expected = 'Intel32' }
+        ) {
+            param ($Assembly, $Expected)
+            $RunAs32Bit = {
+                param($Assembly)
+                Import-Module .\DLLInfo\DLLInfo.psm1 -Force
+                Get-TargetCPU $Assembly
+            }
+
+            $Job = Start-Job $RunAs32Bit -RunAs32 -Arg $Assembly
+            $Job | Wait-Job | Receive-Job | Should -Be $Expected
+        }
+    }
+}
